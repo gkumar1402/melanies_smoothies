2a5ecb209fad4a5522eaa972ecbc44fcf8ea00eb
gkumar1402 <76725390+gkumar1402@users.noreply.github.com>
2024-08-05 19:58:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
+streamlit.title('My Parents New Healthy Diner')
 from snowflake.snowpark.functions import col
 
 
